14d7dbdd291222f78d4a27597e73f3b296049730
PatrickPitts <patrick.pittsj@gmail.com>
2019-05-09 13:35:11 -0700

    revamp form development

--- src/TestGUIFOrm.java
+++ src/TestGUIFOrm.java
@@ -2,7 +2,27 @@ import javax.swing.*;
 
 public class TestGUIFOrm {
     private JLabel chrNameSub;
-    private JTextField textField1;
-    private JTextField textField2;
+    private JTextField classField;
+    private JTextField raceField;
     private JTextField chrNameTextBox;
+    private JTextField backgroundField;
+    private JTextField alignField;
+    private JTextField playerNameField;
+    private JTextField expField;
+    private JTextField strField;
+    private JLabel strengthLabel;
+    private JLabel strModLabel;
+    private JTextField a0TextField;
+    private JTextField textField1;
+    private JCheckBox ___StrengthCheckBox;
+    private JCheckBox checkBox1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+
+    public void buildShowSheet(PlayerCharacter p){
+
+        strModLabel.setText(""+p.getStrMod());
+    }
 }
