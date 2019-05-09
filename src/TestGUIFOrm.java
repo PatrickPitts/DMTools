63e0d6b6585fb63e386c207df9eaed1829f66fe9
PatrickPitts <patrick.pittsj@gmail.com>
2019-05-09 11:29:47 -0700

    using forms

--- src/TestGUIFOrm.java
+++ src/TestGUIFOrm.java
@@ -1,4 +1,8 @@
 import javax.swing.*;
 
 public class TestGUIFOrm {
+    private JLabel chrNameSub;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField chrNameTextBox;
 }
